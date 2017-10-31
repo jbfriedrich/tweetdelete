e1f92759c7ce94d87b2cf9af4eb940a8e3b35936
Jason Friedrich <jason@friedrich>
2017-10-31 22:52:50 +0100

    Fixed formatting issues (linter/flake8)

--- del_likes.py
+++ del_likes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+"""Delete Twitter Likes (old script)."""
 
 import twitter
 import time
 
 api = twitter.Api(
-    consumer_key        = '',
-    consumer_secret     = '',
-    access_token_key    = '',
-    access_token_secret = ''
+    consumer_key='',
+    consumer_secret='',
+    access_token_key='',
+    access_token_secret=''
 )
 
-user    = 'twitterhandle'
-likes   = api.GetFavorites(user_id = user, count = '200')
+user = 'user-id'
+likes = api.GetFavorites(user_id=user, count='200')
 
 for like in likes:
     print "Deleting Like with ID %s" % (like.id)
